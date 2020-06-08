5b0a1c2d0cc147125a3e5e13a88668d32a847f00
Matt Lintz <matt.lintz@gmail.com>
2020-06-07 22:01:42 -0700

    init cleanup
    changing template for base / menu scene first load

---
+++ src/MenuScene.ts
@@ -0,0 +1,19 @@
+
+class MenuScene extends Phaser.Scene {
+
+    constructor() {
+        super({
+            key: 'MenuScene'
+        });
+    }
+
+    create(): void {
+        console.log('menu scene create');
+       
+       
+    }
+
+  
+}
+
+export default MenuScene
--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,24 @@
 import 'phaser';
+import MenuScene from './MenuScene'
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
-    }
-
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
-    }
-
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
-    }
-}
-
-const config = {
+const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
-};
-
-const game = new Phaser.Game(config);
+    parent: 'content',
+    width: window.innerWidth,
+    height: window.innerHeight,
+    resolution: 1,
+    scale: {
+      mode: Phaser.Scale.RESIZE,
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: true
+      } 
+    },
+    backgroundColor: "#000000",
+    scene: [MenuScene]
+  };
+  
+  let game: Phaser.Game = new Phaser.Game(config);
