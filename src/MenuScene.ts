5006b069f39693772eba358d3fa683262b7f563c
Matt Lintz <matt.lintz@gmail.com>
2020-06-07 22:36:55 -0700

    Menu Scene Title
    Not 100% sure on final game name

--- src/MenuScene.ts
+++ src/MenuScene.ts
@@ -1,6 +1,6 @@
 
 class MenuScene extends Phaser.Scene {
-
+    private headerText: Phaser.GameObjects.Text;
     constructor() {
         super({
             key: 'MenuScene'
@@ -9,7 +9,21 @@ class MenuScene extends Phaser.Scene {
 
     create(): void {
         console.log('menu scene create');
-       
+        
+        this.headerText = this.add.text(5, 5, "Simon Hex Memory", { fontFamily: 'Verdana, "Times New Roman", Tahoma, serif', fontSize: 64, color: '#ffffff' })
+        .setTintFill(0xff0000, 0x00ff00, 0xffffff, 0xffffff);
+        this.headerText.setFlipX(false);
+        let headerMove = (this.scale.width-(this.headerText.width))
+        this.tweens.add({
+            targets: this.headerText,
+            x: headerMove,
+            flipX:true,
+            duration: 3000,
+            ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo:true,
+            delay: 500
+        });
        
     }
 
